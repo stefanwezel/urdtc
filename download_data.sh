55645209a59aef18135817e176f07274e40d3b9e
wastedsummer <stefan.wezel@student.uni-tuebingen.de>
2020-11-28 17:29:28 +0100

    final

--- download_data.sh
+++ download_data.sh
@@ -4,15 +4,15 @@
 BASEWD=$(pwd)
 
 # AWA2 Dataset
-#mkdir -p data/awa2
-#cd data/awa2
-#wget https://cvml.ist.ac.at/AwA2/AwA2-data.zip
-#unzip AwA2-data.zip
-#mv Animals_with_Attributes2/* .
-#rm -r Animals_with_Attributes2
-#rm AwA2-data.zip
-#cd $BASEWD
-#python preprocessing/awa_preprocessing.py -data_path data/awa2
+mkdir -p data/awa2
+cd data/awa2
+wget https://cvml.ist.ac.at/AwA2/AwA2-data.zip
+unzip AwA2-data.zip
+mv Animals_with_Attributes2/* .
+rm -r Animals_with_Attributes2
+rm AwA2-data.zip
+cd $BASEWD
+python preprocessing/awa_preprocessing.py -data_path data/awa2
 
 # APY Dataset
 mkdir -p data/apy
@@ -27,13 +27,13 @@ python utils/preprocessing/apy_preprocessing.py -data_path data/apy
 
 # CUB Dataset
 #mkdir -p data/cub
-#cd data/cub
-#wget http://www.vision.caltech.edu/visipedia-data/CUB-200-2011/CUB_200_2011.tgz
-#tar xf CUB_200_2011.tgz
-#mv CUB_200_2011/* .
-#rm -r CUB_200_2011
-#rm CUB_200_2011.tgz
-#cd $BASEWD
+cd data/cub
+wget http://www.vision.caltech.edu/visipedia-data/CUB-200-2011/CUB_200_2011.tgz
+tar xf CUB_200_2011.tgz
+mv CUB_200_2011/* .
+rm -r CUB_200_2011
+rm CUB_200_2011.tgz
+cd $BASEWD
 
 # ImageNet Dataset
 # mkdir -p data/ilsvrc2012
